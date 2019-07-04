e43a406916e048cb8a8391739fdd88a336bb0680
AdityaMehta02 <mr.aditya.mehta@gmail.com>
2019-07-04 17:22:44 +0000

    Added training scripts

--- research/deeplab/goat_test_mobilenetv2.sh
+++ research/deeplab/goat_test_mobilenetv2.sh
@@ -14,12 +14,12 @@
 # limitations under the License.
 # ==============================================================================
 #
-# This script is used to run local test on PASCAL VOC 2012. Users could also
-# modify from this script for their use case.
+# This script is used to run local test on PASCAL VOC 2012 using MobileNet-v2.
+# Users could also modify from this script for their use case.
 #
 # Usage:
 #   # From the tensorflow/models/research/deeplab directory.
-#   sh ./local_test.sh
+#   sh ./local_test_mobilenetv2.sh
 #
 #
 
@@ -43,14 +43,13 @@ python "${WORK_DIR}"/model_test.py -v
 DATASET_DIR="datasets"
 cd "${WORK_DIR}/${DATASET_DIR}"
 sh download_and_convert_goatVOC.sh
-#tar xvf goat_molt_seg.tar
 
 # Go back to original directory.
 cd "${CURRENT_DIR}"
 
 # Set up the working directories.
 PASCAL_FOLDER="goat_molt_seg"
-EXP_FOLDER="exp/train_on_trainval_set"
+EXP_FOLDER="exp/train_on_trainval_set_mobilenetv2"
 INIT_FOLDER="${WORK_DIR}/${DATASET_DIR}/${PASCAL_FOLDER}/init_models"
 TRAIN_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${PASCAL_FOLDER}/${EXP_FOLDER}/train"
 EVAL_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${PASCAL_FOLDER}/${EXP_FOLDER}/eval"
@@ -64,7 +63,8 @@ mkdir -p "${EXPORT_DIR}"
 
 # Copy locally the trained checkpoint as the initial checkpoint.
 TF_INIT_ROOT="http://download.tensorflow.org/models"
-TF_INIT_CKPT="deeplabv3_pascal_train_aug_2018_01_04.tar.gz"
+CKPT_NAME="deeplabv3_mnv2_pascal_train_aug"
+TF_INIT_CKPT="${CKPT_NAME}_2018_01_29.tar.gz"
 cd "${INIT_FOLDER}"
 wget -nd -c "${TF_INIT_ROOT}/${TF_INIT_CKPT}"
 tar -xf "${TF_INIT_CKPT}"
@@ -77,33 +77,24 @@ NUM_ITERATIONS=10000
 python "${WORK_DIR}"/train.py \
   --logtostderr \
   --train_split="trainval" \
-  --model_variant="xception_65" \
-  --atrous_rates=6 \
-  --atrous_rates=12 \
-  --atrous_rates=18 \
+  --model_variant="mobilenet_v2" \
   --output_stride=16 \
-  --decoder_output_stride=4 \
   --train_crop_size="513,513" \
   --train_batch_size=4 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
   --fine_tune_batch_norm=true \
-  --tf_initial_checkpoint="${INIT_FOLDER}/deeplabv3_pascal_train_aug/model.ckpt" \
+  --tf_initial_checkpoint="${INIT_FOLDER}/${CKPT_NAME}/model.ckpt-30000" \
   --train_logdir="${TRAIN_LOGDIR}" \
   --dataset="${PASCAL_FOLDER}" \
   --dataset_dir="${PASCAL_DATASET}"
 
 # Run evaluation. This performs eval over the full val split (1449 images) and
 # will take a while.
-# Using the provided checkpoint, one should expect mIOU=82.20%.
+# Using the provided checkpoint, one should expect mIOU=75.34%.
 python "${WORK_DIR}"/eval.py \
   --logtostderr \
   --eval_split="val" \
-  --model_variant="xception_65" \
-  --atrous_rates=6 \
-  --atrous_rates=12 \
-  --atrous_rates=18 \
-  --output_stride=16 \
-  --decoder_output_stride=4 \
+  --model_variant="mobilenet_v2" \
   --eval_crop_size="513,513" \
   --checkpoint_dir="${TRAIN_LOGDIR}" \
   --eval_logdir="${EVAL_LOGDIR}" \
@@ -115,12 +106,7 @@ python "${WORK_DIR}"/eval.py \
 python "${WORK_DIR}"/vis.py \
   --logtostderr \
   --vis_split="val" \
-  --model_variant="xception_65" \
-  --atrous_rates=6 \
-  --atrous_rates=12 \
-  --atrous_rates=18 \
-  --output_stride=16 \
-  --decoder_output_stride=4 \
+  --model_variant="mobilenet_v2" \
   --vis_crop_size="513,513" \
   --checkpoint_dir="${TRAIN_LOGDIR}" \
   --vis_logdir="${VIS_LOGDIR}" \
@@ -136,12 +122,7 @@ python "${WORK_DIR}"/export_model.py \
   --logtostderr \
   --checkpoint_path="${CKPT_PATH}" \
   --export_path="${EXPORT_PATH}" \
-  --model_variant="xception_65" \
-  --atrous_rates=6 \
-  --atrous_rates=12 \
-  --atrous_rates=18 \
-  --output_stride=16 \
-  --decoder_output_stride=4 \
+  --model_variant="mobilenet_v2" \
   --num_classes=4 \
   --crop_size=513 \
   --crop_size=513 \
